82f33dc28bc119ceca01bf961c6adeddd2d6247c
Thomas Christopher <thomas.j.christopher@gmail.com>
2019-08-29 16:35:00 +0100

    Allow gameplay until full board

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 class Board
+  attr_reader :available_cells
   def initialize
     @cells = [1,2,3,4,5,6,7,8,9]
+    @available_cells = [1,2,3,4,5,6,7,8,9]
   end
 
   def display
@@ -16,6 +18,11 @@ class Board
   def update_cells(num, weapon)
     # system("cls") || system("clear")
     @cells[num-1] = weapon
+    available_cells.delete(num)
+    display
+  end
+
+  def to_s
     display
   end
 end
@@ -25,35 +32,73 @@ class Game
   def initialize
     @board = Board.new
     @board.display
+    puts "Player 1, choose X/O?"
+    p1_selection = gets.chomp
+    self.set_player_one p1_selection.upcase
+    puts "Player one chose: #{@player_one}"
+    if @player_one.weapon == "X"
+      self.set_player_two "O"
+    else
+      self.set_player_two "X"
+    end
+    puts "Player two you are: #{@player_two}"
   end
 
   def set_player_one(weapon)
-    @player_one = Player.new(weapon)
+    @player_one = Player.new(weapon, @board)
+    @player_one.game = self
   end
   
   def set_player_two(weapon)
-    @player_two = Player.new(weapon)
+    @player_two = Player.new(weapon, @board)
+    @player_two.game = self
+  end
+
+  def get_player_move(player)
+    puts "Player #{player}, choose your move..."
+    move = gets.chomp.to_i
+    puts "Player #{player}, chose #{move}"
+    player.choose_move move
   end
 end
 
 class Player
   attr_reader :weapon
-  def initialize(weapon)
+  def initialize(weapon, board)
     @weapon = weapon
+    @board = board
   end
+
+  def choose_move(choice)
+    if @board.available_cells.include? choice
+      @board.update_cells(choice, self.weapon)
+    else
+      puts "This cell is already taken, please choose again"
+      @game.get_player_move(self)
+    end
+  end
+
+  def game=(game)
+    @game = game
+  end
+
   def to_s
     @weapon
   end
 end
 
 game = Game.new
-puts "Player 1, choose X/O?"
-p1_selection = gets.chomp
-game.set_player_one p1_selection.upcase
-if game.player_one.weapon == "X"
-  game.set_player_two "O"
-else
-  game.set_player_two "X"
+
+
+while true
+  unless game.board.available_cells.empty?
+    game.get_player_move game.player_one
+    unless game.board.available_cells.empty?
+      game.get_player_move game.player_two
+    else
+      break
+    end
+  else
+    break
+  end
 end
-game.board.update_cells 5, game.player_one
-game.board.update_cells 6, game.player_two
